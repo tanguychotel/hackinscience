0c4f51b836aae498cb3251ab648e336c86dfa588
Tanguy Chotel <tanguy.chotel@cri-paris.org>
2015-11-06 17:03:58 +0100

    rt

--- exercises/025/solution.py
+++ exercises/025/solution.py
@@ -1,3 +1,3 @@
 import datetime
 print("Today is"), (datetime.datetime.now().date()), ("and it is"),\
-(datetime.datetime.now().strftime("%H:%M:%S"))
+    (datetime.datetime.now().strftime("%H:%M:%S"))
