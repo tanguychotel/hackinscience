f814fb5d2aff6f6a271dc2eb8fd26419f21cca24
Tanguy Chotel <tanguy.chotel@cri-paris.org>
2015-11-06 16:09:26 +0100

    rt

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -1,8 +1,10 @@
 import sys
-x=int(sys.argv[1])
-y=int(sys.argv[2])
-if len(sys.argv) > 2:
-    print(x + y)
-else:
-    print("usage: python3 solution.py PARAM")
 
+if len(sys.argv) <= 2:
+    print("usage: python3 solution.py PARAM")
+else:
+    x=int(sys.argv[1])
+    y=int(sys.argv[2])
+    print(x + y)
+
+   
