52b9c7502fc2d0229d4c452304662fe38f829b83
Tanguy Chotel <tanguy.chotel@cri-paris.org>
2015-11-20 16:45:58 +0100

    rt

---
+++ exercises/085/solution.py
@@ -0,0 +1,15 @@
+from operator import itemgetter, attrgetter
+
+
+def sort_a_list(l):
+    a=(sorted(l, reverse = True))
+    return(a)
+def sort_a_mark(my_class):
+    b=(sorted(my_class, reverse = True))
+    return(b)
+
+def sort_by_name(my_class):
+    getcount = itemgetter(1)
+    c=(sorted(my_class, key=getcount))
+    return(c)
+
