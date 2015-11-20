a52b5dbb015874636cb59eff28c668285958ac91
Tanguy Chotel <tanguy.chotel@cri-paris.org>
2015-11-20 16:51:06 +0100

    rt

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -2,14 +2,14 @@ from operator import itemgetter, attrgetter
 
 
 def sort_a_list(l):
-    a = (sorted(l, reverse = True))
+    a = (sorted(l, reverse=True))
     return(a)
 
 def sort_a_mark(my_class):
-    b = (sorted(my_class, reverse = True))
+    b = (sorted(my_class, reverse=True))
     return(b)
 
 def sort_by_name(my_class):
     getcount = itemgetter(1)
-    c = (sorted(my_class, key = getcount))
+    c = (sorted(my_class, key=getcount))
     return(c)
