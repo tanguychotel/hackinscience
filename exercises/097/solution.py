ca0a9ea9f8cc8534b57b9d050b1a030695427b20
Tanguy Chotel <tanguy.chotel@cri-paris.org>
2015-11-27 17:00:55 +0100

    rt

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -3,3 +3,12 @@ def love_meet(alice, bob):
     bob2 = set(bob)
     b = [val for val in alice2 if val in bob2]
     return(b)
+
+
+def affair_meet(alice, bob, silvester):
+    alice2 = set(alice)
+    bob2 = set(bob)
+    silvester2 = set(silvester)
+    c = [val for val in alice2 if val in silvester2]
+    d = [val for val in c if val not in bob2]
+    return(d)
